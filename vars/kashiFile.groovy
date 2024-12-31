c9c51a34b1191a7f102644b00ca6532e5f3c275a
Sri Sai Venkata Kasi Subbarayudu Kompella <kasisubbarayudu@gmail.com>
2024-12-31 15:11:58 +0530

    Make jenkinsfile resuable
    Signed-off-by: Sri Sai Venkata Kasi Subbarayudu Kompella <kasisubbarayudu@gmail.com>

---
+++ vars/kashiFile.groovy
@@ -0,0 +1,27 @@
+
+def call(){
+pipeline{
+        agent any
+        stages{
+            stage("Test stage - File"){
+                steps{
+                    script{
+                         kashi()
+                         println "This statement has been added to trigger the changeset"
+                         println "New print..."
+                    }
+                }
+            }
+            stage("Changeset"){
+             when{
+               changeset "**/README.md"
+             }
+             steps{
+               script{
+                  println "The change was triggered.."
+               }
+             }
+           }
+        }
+    }
+}
